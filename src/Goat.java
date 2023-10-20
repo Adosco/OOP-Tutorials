98958070a760a1275be4e87bce1b1929f145730e
Emmanuel Adovor <adovoremmanuel8@gmail.com>
2023-10-20 23:56:52 +0000

    Modification

--- src/Goat.java
+++ src/Goat.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Goat {
+public class Goat extends Animals{
+    String color;
+    public Goat(String name,String color){
+        super(name);
+        this.color = color;
+    }
+
+    @Override
+    public void makeSound(){
+        System.out.println("Meeeeeiii");
+    }
+
+    @Override
+    public String toString() {
+        return "Goat{" +
+                "color='" + color + '\'' +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args){
+        Goat goat = new Goat("Joseph","Black");
+
+        goat.makeSound();
+        System.out.println(goat.getName());
+
+        System.out.println(goat);
 
     }
 
